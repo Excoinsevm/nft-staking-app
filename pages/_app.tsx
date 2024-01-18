b2762844ab983211644d24b8888cb495e2aed3bf
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-01-18 19:54:32 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,12 @@ import type { AppProps } from "next/app";
 import CustomChain from "../consts/chain";
 import "../styles/globals.css";
 
-// This is the chain your dApp will work on.
-const activeChain = "CustomChain";
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       activeChain={activeChain}
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+      activeChain={CustomChain}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
